ccae8f13ccbc30a8a7f6a0aa26af17293be4054c
TsereteliPh <filipptsereteli@yandex.ru>
2024-03-13 13:08:48 +0300

    add and done header

--- assets/js/main.js
+++ assets/js/main.js
@@ -289,33 +289,33 @@ if (clientsCarousel) {
 
 // Функционал шапки сайта
 
-// document.addEventListener("DOMContentLoaded", function(e) {
-// 	const header = document.querySelector('.header');
+document.addEventListener("DOMContentLoaded", function(e) {
+	const header = document.querySelector('.header');
 
-// 	if (header) {
-// 		const headerBurger = header.querySelector('.header__burger');
-// 		const headerDrop = header.querySelector('.header__drop');
+	if (header && window.innerWidth <= 768) {
+		const headerBurger = header.querySelector('.header__burger');
+		const headerMenu = header.querySelector('.header__menu');
 
-// 		const dropOpener = () => {
-// 			header.classList.add('active');
-// 			headerBurger.classList.add('active');
-// 			headerDrop.style.maxHeight = headerDrop.scrollHeight + "px";
-// 			document.body.style.overflow = 'hidden';
-// 		}
+		const menuOpener = () => {
+			header.classList.add('active');
+			headerBurger.classList.add('active');
+			headerMenu.style.maxHeight = headerMenu.scrollHeight + "px";
+			document.body.style.overflow = 'hidden';
+		}
 
-// 		const dropCloser = () => {
-// 			header.classList.remove('active');
-// 			headerBurger.classList.remove('active');
-// 			headerDrop.style.maxHeight = 0;
-// 			document.body.style.overflow = 'visible';
-// 		}
+		const menuCloser = () => {
+			header.classList.remove('active');
+			headerBurger.classList.remove('active');
+			headerMenu.style.maxHeight = 0;
+			document.body.style.overflow = 'visible';
+		}
 
-// 		headerBurger.addEventListener('click', function() {
-// 			if (this.classList.contains('active')) {
-// 				dropCloser();
-// 			} else {
-// 				dropOpener();
-// 			}
-// 		})
-// 	}
-// })
+		headerBurger.addEventListener('click', function() {
+			if (this.classList.contains('active')) {
+				menuCloser();
+			} else {
+				menuOpener();
+			}
+		})
+	}
+})
--- header.php
+++ header.php
@@ -12,6 +12,53 @@
 <?php wp_body_open(); ?>
 
 <header class="header">
+	<div class="container header__container">
+		<a href="<?php echo bloginfo( 'url' ); ?>" class="neon-text header__logo" arai-label="Вернуться на главную страницу">Bugs bunny</a>
 
+		<nav class="header__menu">
+			<a href="#" class="header__menu-item active">О нас</a>
+			<a href="#" class="header__menu-item">Заказ</a>
+			<a href="#" class="header__menu-item">Скидки</a>
+			<a href="#" class="header__menu-item">Меню</a>
+			<a href="#" class="header__menu-item">Контакты</a>
+		</nav>
+
+		<?php
+			$address = get_field( 'address', 'options' );
+			$tel = get_field( 'tel', 'options' );
+			$socials = get_field( 'socials', 'options' );
+
+			if ( $address || $tel || $socials ) :
+				?>
+					<div class="header__contacts">
+						<div class="header__info">
+							<?php if ( $address ) : ?>
+								<address class="header__address"><?php echo $address; ?></address>
+							<?php endif; ?>
+
+							<?php if ( $tel ) : ?>
+								<a href="tel:<?php echo preg_replace( '/[^0-9,+]/', '', $tel ); ?>" class="header__phone"><?php echo $tel; ?></a>
+							<?php endif; ?>
+						</div>
+
+						<?php if ( $socials ) : ?>
+							<div class="header__socials socials">
+								<?php foreach ( $socials as $social ) : ?>
+									<a href="<?php echo $social['link']; ?>" class="neon-circle socials__item">
+										<svg width="22" height="22"><use xlink:href="<?php echo get_template_directory_uri(); ?>/assets/images/sprite.svg#icon-<?php echo $social['icon']; ?>"></use></svg>
+									</a>
+								<?php endforeach; ?>
+							</div>
+						<?php endif; ?>
+					</div>
+				<?php
+			endif;
+		?>
+
+		<button class="header__burger" type="button" aria-label="Открыть/закрыть меню">
+			<span></span>
+		</button>
+	</div>
 </header>
+
 <main class="main">
