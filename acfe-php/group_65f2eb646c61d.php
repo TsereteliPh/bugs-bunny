08aa45408f8aa75e96ece8b07abacb1c9e8e65be
TsereteliPh <filipptsereteli@yandex.ru>
2024-03-14 18:46:44 +0300

    add and done .rent

---
+++ acfe-php/group_65f2eb646c61d.php
@@ -0,0 +1,107 @@
+<?php 
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group(array(
+	'key' => 'group_65f2eb646c61d',
+	'title' => 'Block:rent',
+	'fields' => array(
+		array(
+			'key' => 'field_65f2eb847a338',
+			'label' => 'Заголовок',
+			'name' => 'title',
+			'aria-label' => '',
+			'type' => 'text',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'default_value' => '',
+			'maxlength' => '',
+			'placeholder' => '',
+			'prepend' => '',
+			'append' => '',
+			'acfe_field_group_condition' => 0,
+		),
+		array(
+			'key' => 'field_65f2eb977a339',
+			'label' => 'Список',
+			'name' => 'list',
+			'aria-label' => '',
+			'type' => 'repeater',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'acfe_repeater_stylised_button' => 0,
+			'layout' => 'table',
+			'pagination' => 0,
+			'min' => 0,
+			'max' => 0,
+			'collapsed' => '',
+			'button_label' => 'Добавить пункт',
+			'acfe_field_group_condition' => 0,
+			'rows_per_page' => 20,
+			'sub_fields' => array(
+				array(
+					'key' => 'field_65f2eba67a33a',
+					'label' => 'Текст пункта',
+					'name' => 'text',
+					'aria-label' => '',
+					'type' => 'text',
+					'instructions' => '',
+					'required' => 0,
+					'conditional_logic' => 0,
+					'wrapper' => array(
+						'width' => '',
+						'class' => '',
+						'id' => '',
+					),
+					'default_value' => '',
+					'maxlength' => '',
+					'placeholder' => '',
+					'prepend' => '',
+					'append' => '',
+					'acfe_field_group_condition' => 0,
+					'parent_repeater' => 'field_65f2eb977a339',
+				),
+			),
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'post',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => false,
+	'description' => '',
+	'show_in_rest' => 0,
+	'acfe_autosync' => array(
+		0 => 'php',
+	),
+	'acfe_form' => 0,
+	'acfe_display_title' => '',
+	'acfe_meta' => '',
+	'acfe_note' => '',
+	'modified' => 1710419265,
+));
+
+endif;
---
+++ layouts/blocks/rent/template.php
@@ -0,0 +1,25 @@
+<section class="rent">
+	<div class="container rent__container">
+		<div class="glass rent__content">
+			<?php if ( get_sub_field( 'title' ) ) : ?>
+				<h2 class="rent__title"><?php the_sub_field( 'title' ); ?></h2>
+			<?php endif; ?>
+
+
+			<?php
+				$list = get_sub_field( 'list' );
+				if ( $list ) :
+					?>
+						<ul class="reset-list rent__list">
+							<?php foreach ( $list as $item ) : ?>
+								<li class="rent__item"><?php echo $item['text']; ?></li>
+							<?php endforeach; ?>
+						</ul>
+					<?php
+				endif;
+			?>
+
+			<button class="btn rent__button" type="button">Арендовать</button>
+		</div>
+	</div>
+</section>
