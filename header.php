7ae3a6a21ef327d63a2f590619dd5debcdc7c133
TsereteliPh <filipptsereteli@yandex.ru>
2024-05-13 11:04:31 +0300

    change font

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.png" type="image/x-icon" width="16" height="16">
 	<?php wp_head(); ?>
 </head>
 
